18976de4b5b94322203324a956c2bb5a5e0e913f
liujiangang <1455629212@qq.com>
2016-10-24 10:23:31 +0800

    单例模式

--- _global.py
+++ _global.py
@@ -4,7 +4,24 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from conf_injection import init_config
+from patterns import singleton
+import ConfigParser
+import os
+
+
+def read_config(file_name):
+    config_path = os.path.join(os.path.dirname(__file__), "conf")
+    file_name = os.path.join(config_path, file_name)
+    config = ConfigParser.ConfigParser()
+    with open(file_name) as f:
+        config.readfp(f)
+    return config
+
+
+@singleton
+def init_config():
+    config = read_config("project.ini")
+    return config
 
 
 config = init_config()

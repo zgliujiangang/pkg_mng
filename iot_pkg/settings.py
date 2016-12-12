c1f8e3970f2b243c492cd9cc261760791fe57f1d
liujiangang <1455629212@qq.com>
2016-12-12 14:26:50 +0800

    fix

--- iot_pkg/settings.py
+++ iot_pkg/settings.py
@@ -9,4 +9,4 @@ USER_SESSION_TIMEOUT = 60 * 60 * 2
 CACHE_PATH = '/Users/admin/Project/iot_pkg/cache_dir'
 FILE_PATH = '/Users/admin/Project/iot_pkg/files'
 DEFAULT_PAGE = 1
-PER_PAGE = 3
+PER_PAGE = 10

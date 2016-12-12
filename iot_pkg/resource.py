636fd48063bfe780b0ff7e5222d4ab6797c7b8e8
admin <admin@Liu.local>
2016-12-12 15:43:20 +0800

    prefix api

--- iot_pkg/resource.py
+++ iot_pkg/resource.py
@@ -8,17 +8,17 @@ from iot_pkg.resources import user, file, proj, pkg
 def add_resource():
     api = create_api()
     # 用户注册、修改密码、获取基本信息接口
-    api.add_resource(user.UserAPI, '/user')
+    api.add_resource(user.UserAPI, '/api/user')
     # 用户获取登录凭证、删除登录凭证接口
-    api.add_resource(user.UserTokenAPI, '/user/token')
+    api.add_resource(user.UserTokenAPI, '/api/user/token')
     # 文件上传下载接口
-    api.add_resource(file.FileAPI, "/file", "/file/<file_id>")
+    api.add_resource(file.FileAPI, "/api/file", "/file/<file_id>")
     # 项目创建、删除、获取信息等接口
-    api.add_resource(proj.ProjectAPI, "/project")
-    api.add_resource(proj.ProjectListAPI, "/project/list")
-    api.add_resource(proj.ProjectMsgAPI, "/project/msg/<uid>", endpoint="project_msg")
-    api.add_resource(proj.ProjectFileAPI, "/project/download/<uid>", endpoint="project_download")
+    api.add_resource(proj.ProjectAPI, "/api/project")
+    api.add_resource(proj.ProjectListAPI, "/api/project/list")
+    api.add_resource(proj.ProjectMsgAPI, "/api/project/msg/<uid>", endpoint="project_msg")
+    api.add_resource(proj.ProjectFileAPI, "/api/project/download/<uid>", endpoint="project_download")
     # 安装包上传、获取、删除等接口
-    api.add_resource(pkg.PackageAPI, "/package")
-    api.add_resource(pkg.PackageListAPI, "/package/list")
-    api.add_resource(pkg.PackageFileAPI, "/package/file", "/package/file/<fid>", endpoint='package_download')
+    api.add_resource(pkg.PackageAPI, "/api/package")
+    api.add_resource(pkg.PackageListAPI, "/api/package/list")
+    api.add_resource(pkg.PackageFileAPI, "/api/package/file", "/api/package/file/<fid>", endpoint='package_download')
